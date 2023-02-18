06afa45200f0a2780573f1df27cd37324b519287
Simgoo <vineethpateel@gmail.com>
2023-02-18 11:48:51 -0500

    Changed ButtonConstants Created SimplCmnd Folder

--- src/main/java/frc/robot/Constants/ButtonConstants.java
+++ src/main/java/frc/robot/Constants/ButtonConstants.java
@@ -6,33 +6,25 @@ public class ButtonConstants {
   public static final int BUTTON_PANEL_PORT = 1;
   /*
    * Button Panel Mapping
-   * -------------------
-   * |    ^    | 1 | 2 |
-   * |   < >   | 3 | 4 |
-   * |    v    | 5 | 6 |
-   * |  9 | 10 | 7 | 8 |
-   * | 11 | 12 |   |   |
-   * -------------------
+   * ----------------------------
+   * |    ^    | 1 | 2 | 3 | 4
+   * |   < >   | 5 | 6 | 7 | 8
+   * |    v    | 9 | 10 |         
+   * ---------------------------
    */
 
   // All Constants are subject to change once we figure out what each one does in the old code. 
   public static final int TURN_TURRET_LEFT = 1;
   public static final int TURN_TURRET_RIGHT = 2;
 
-  public static final int INTAKE_EXTEND = 3;
-  public static final int INTAKE_RETRACT = 4;
+  public static final int ARM_EXTEND = 3;
+  public static final int ARM_RETRACT = 4;
 
-  public static final int INDEX_UP = 5;
-  public static final int INDEX_OUT = 6;
+  public static final int CLAW_EXTEND = 5;
+  public static final int CLAW_RETRACT = 6;
 
-  public static final int INTAKE_IN = 7;
-  public static final int INTAKE_OUT = 8;
+  public static final int VBAR_UP = 7;
+  public static final int VBAR_DOWN = 8;
   public static final int SHOOT_BUTTON = 9;
-  public static final int SHOOTFALLBACK = 10;
-
-  public static final int FLYWHEEL_ON = 11;
-  public static final int SHOOT_WRONG_BALL = 12;
-
-  public static final int TARGET_SHOOTER = 12;
-    
+  public static final int SHOOTFALLBACK = 10;    
 }
