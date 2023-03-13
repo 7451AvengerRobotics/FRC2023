2b61fac0ca4543cbfc812854d6bdd266e82c800d
AvengerRobotics7451 <118380153+avenger-robotics@users.noreply.github.com>
2023-03-13 15:27:49 -0400

    SoftLimits vfbar

--- src/main/java/frc/robot/subsystems/VirtualFourBar.java
+++ src/main/java/frc/robot/subsystems/VirtualFourBar.java
@@ -3,10 +3,12 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PortConstants;
+import frc.robot.Constants.TurretConstants;
 
 public class VirtualFourBar extends SubsystemBase {
 
@@ -16,8 +18,18 @@ public class VirtualFourBar extends SubsystemBase {
         vFBAR = new WPI_TalonFX(PortConstants.VirtualFourBar);
         vFBAR.setNeutralMode(NeutralMode.Brake);
         vFBAR.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
-    }
+        
+    
+        
 
+        vFBAR.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, TurretConstants.kTimeoutMs);
+        vFBAR.configForwardSoftLimitThreshold(98590);
+        vFBAR.configReverseSoftLimitThreshold(-300);//Values subject to change
+        vFBAR.configForwardSoftLimitEnable(true);
+        vFBAR.configReverseSoftLimitEnable(true);
+        //Directly forward is 46511
+
+    }
 
 
     public void setPower(double power){
@@ -31,6 +43,10 @@ public class VirtualFourBar extends SubsystemBase {
     
     public double getencoderValues() {
         return vFBAR.getSelectedSensorPosition();
+    }
+
+    public void zeroSensors() {
+        vFBAR.getSensorCollection().setIntegratedSensorPosition(0, TurretConstants.kTimeoutMs);
     }
 
 }
