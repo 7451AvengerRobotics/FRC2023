7c8673eda2b0a3da2bb35af1466a3b7d0797e7dd
sraje <srajen2018@gmail.com>
2023-03-16 19:08:52 -0400

    Auto please work

--- src/main/java/frc/robot/Constants/AutoConstants.java
+++ src/main/java/frc/robot/Constants/AutoConstants.java
@@ -3,7 +3,11 @@ package frc.robot.Constants;
 import java.util.HashMap;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoConstants {
     public static final HashMap<String, Command> basicChargeAuto = new HashMap<>();
+    public static final HashMap<String, Command> twoCubeAuto = new HashMap<>();
+    public static void twoCubeAuto(String string, SequentialCommandGroup sequentialCommandGroup) {
+    }
 }
