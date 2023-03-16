89dd0bb4a66a1b189561a7c974762ec6940fe21a
AntNeedsHelp <anthony@novokshanov.com>
2023-03-16 16:05:56 -0400

    i hate all kids

--- src/main/java/frc/robot/commands/AutoCommands/BalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoCommands/BalanceCommand.java
@@ -2,9 +2,7 @@ package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.subsystems.AutoBalance;
 import frc.robot.subsystems.Drivetrain;
 
 public class BalanceCommand extends CommandBase {
