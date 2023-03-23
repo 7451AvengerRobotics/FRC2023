dba9516b423d565babf4b24625f722d2449071ce
AvengerRobotics <avengerrobotics7451@gmail.com>
2023-03-23 18:43:55 -0500

    Carrolton Day 1

--- src/main/java/frc/robot/commands/AutoCommands/TwoCubeAuto.java
+++ src/main/java/frc/robot/commands/AutoCommands/TwoCubeAuto.java
@@ -20,11 +20,11 @@ public class TwoCubeAuto extends SequentialCommandGroup {
   public TwoCubeAuto(Arm arm, Drivetrain drive, double drivepower, VirtualFourBar bar, Claw claw, double clawpower, Turret turret, Pigeon2 gyro) {
     addCommands(
       Commands.sequence(
-        new TurretTestCommand(turret, 0.5).withTimeout(1.30),
-        new ArmExtendCommand(arm).withTimeout(2),
-        new ClawOuttake(claw, clawpower).withTimeout(1),
-        new ArmRetractCommand(arm).withTimeout(3),
-        new TurretTestCommand(turret, -0.5).withTimeout(1.4),
+        //new TurretTestCommand(turret, 0.5).withTimeout(1.30),
+        //new ArmExtendCommand(arm).withTimeout(2),
+        //new ClawOuttake(claw, clawpower).withTimeout(1),
+        //new ArmRetractCommand(arm).withTimeout(3),
+        //new TurretTestCommand(turret, -0.5).withTimeout(1.4),
         new DriveBackAuto(drive, drivepower).withTimeout(1.5)),
         new GetOnRamp(drive).withTimeout(3),
         new BalanceCommand(drive).withTimeout(5)
